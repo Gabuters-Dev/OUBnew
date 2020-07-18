2c84dd081f5ddea27a4b5071b22347cc2a718e62
Heinz Der Flugel <heinzdf@outlook.com>
2020-07-18 09:12:40 +0800

    events: Send error to botlog if available
    Signed-off-by: Heinz Der Flugel <heinzdf@outlook.com>

--- userbot/events.py
+++ userbot/events.py
@@ -12,12 +12,12 @@ from asyncio import subprocess as asyncsub
 from os import remove
 from time import gmtime, strftime
 from traceback import format_exc
-
+ 
 from telethon import events
-
+ 
 from userbot import bot, BOTLOG_CHATID, LOGSPAMMER
-
-
+ 
+ 
 def register(**args):
     """ Register a new event. """
     pattern = args.get('pattern', None)
@@ -28,32 +28,32 @@ def register(**args):
     trigger_on_fwd = args.get('trigger_on_fwd', False)
     disable_errors = args.get('disable_errors', False)
     insecure = args.get('insecure', False)
-
+ 
     if pattern is not None and not pattern.startswith('(?i)'):
         args['pattern'] = '(?i)' + pattern
-
+ 
     if "disable_edited" in args:
         del args['disable_edited']
-
+ 
     if "ignore_unsafe" in args:
         del args['ignore_unsafe']
-
+ 
     if "groups_only" in args:
         del args['groups_only']
-
+ 
     if "disable_errors" in args:
         del args['disable_errors']
-
+ 
     if "trigger_on_fwd" in args:
         del args['trigger_on_fwd']
-
+ 
     if "insecure" in args:
         del args['insecure']
-
+ 
     if pattern:
         if not ignore_unsafe:
             args['pattern'] = pattern.replace('^.', unsafe_pattern, 1)
-
+ 
     def decorator(func):
         async def wrapper(check):
             if check.edit_date and check.is_channel and not check.is_group:
@@ -64,17 +64,17 @@ def register(**args):
                 send_to = check.chat_id
             else:
                 send_to = BOTLOG_CHATID
-
+ 
             if not trigger_on_fwd and check.fwd_from:
                 return
-
+ 
             if groups_only and not check.is_group:
                 await check.respond("`I don't think this is a group.`")
                 return
-
+ 
             if check.via_bot_id and not insecure and check.out:
                 return
-
+ 
             try:
                 await func(check)
 
@@ -97,10 +97,10 @@ def register(**args):
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
                     text = "**USERBOT ERROR REPORT**\n"
-                    link = "Support chat PM: @fortifying"
+                    link = "[me](https://t.me/heinzdf)"
                     text += "If you want to, you can report it"
                     text += f"- just forward this message to {link}.\n"
-                    text += "Nothing is logged except the fact of error and date\n"
+                    text += "Nothing is logged except the fact of error and date.\n"
 
                     ftext = "========== DISCLAIMER =========="
                     ftext += "\nThis file uploaded ONLY here,"
@@ -132,29 +132,29 @@ def register(**args):
                     stdout, stderr = await process.communicate()
                     result = str(stdout.decode().strip()) \
                         + str(stderr.decode().strip())
-
+ 
                     ftext += result
-
+ 
                     file = open("error.log", "w+")
                     file.write(ftext)
                     file.close()
-
+ 
                     if LOGSPAMMER:
-                        await check.client.respond(
-                            "`Sorry, my userbot has crashed."
-                            "\nThe error logs are stored in the userbot's log chat.`"
+                        await check.respond(
+                            "`Sorry, my userbot has crashed.\
+                        \nThe error logs are stored in the userbot's log chat.`"
                         )
-
+ 
                     await check.client.send_file(send_to,
                                                  "error.log",
                                                  caption=text)
                     remove("error.log")
             else:
                 pass
-
+ 
         if not disable_edited:
             bot.add_event_handler(wrapper, events.MessageEdited(**args))
         bot.add_event_handler(wrapper, events.NewMessage(**args))
         return wrapper
-
+ 
     return decorator
